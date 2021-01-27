a0b8b315a6624080274ce850f5274ca132618a78
Jörg Thalheim <joerg@thalheim.io>
2021-01-27 21:00:32 +0100

    fix checksum

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ pkgs.rustPlatform.buildRustPackage {
   pname = "envfs";
   version = "0.0.1";
   src = ./.;
-  cargoSha256 = "sha256-Fma8r1WwyvUmM+BHMLqHRaI5xJhh9cBA9r7YXifwJjU=";
+  cargoSha256 = "sha256-sxHYUkXf5OIAIZG2VKmTg9qCuDi146Uhzxdv3wF4Fzw=";
 
   postInstall = ''
     ln -s envfs $out/bin/mount.envfs
