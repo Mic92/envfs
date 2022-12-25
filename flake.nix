1522ec13ecb7bacedffece0d89e0134cc2415d4b
Jörg Thalheim <joerg@thalheim.io>
2022-12-25 15:27:21 +0100

    switch from flake-utils to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,38 +1,38 @@
 {
   description = "Fuse filesystem that returns symlinks to executables based on the PATH of the requesting process.";
 
-  inputs.utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, utils }: {
-    nixosModules.envfs = import ./modules/envfs.nix;
-  } // utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages = {
-        envfs = pkgs.callPackage ./default.nix {
-          packageSrc = self;
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({self, ...}: {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "riscv64-linux"
+      ];
+      flake.envfs = import ./modules/envfs.nix;
+      perSystem = { self', pkgs, ... }: {
+        packages = {
+          envfs = pkgs.callPackage ./default.nix {
+            packageSrc = self;
+          };
+          envfsStatic = pkgs.pkgsStatic.callPackage ./default.nix {
+            packageSrc = self;
+          };
+          default = self'.packages.envfs;
         };
-        envfsStatic = pkgs.pkgsStatic.callPackage ./default.nix {
-          packageSrc = self;
+        checks = {
+          envfsCrossAarch64 = pkgs.pkgsCross.aarch64-multiplatform.callPackage ./default.nix {
+            packageSrc = self;
+          };
+          integration-tests = import ./nixos-test.nix {
+            makeTest = import (inputs.nixpkgs + "/nixos/tests/make-test-python.nix");
+            inherit pkgs;
+            inherit (self'.packages) cntr;
+          };
         };
-        default = self.packages.${system}.envfs;
       };
-    }) // {
-      checks.x86_64-linux = let
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        envfsCrossAarch64 = pkgs.pkgsCross.aarch64-multiplatform.callPackage ./default.nix {
-          packageSrc = self;
-        };
-        integration-tests = import ./nixos-test.nix {
-          makeTest = import (nixpkgs + "/nixos/tests/make-test-python.nix");
-          inherit pkgs;
-          inherit (self.packages.${system}) cntr;
-        };
-      };
-  };
+    });
 }
