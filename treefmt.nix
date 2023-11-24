ee19b391d7599b4f632b7fe7b5ea40a50e5003d5
Jörg Thalheim <joerg@thalheim.io>
2023-11-24 11:28:32 +0100

    fix eval

--- treefmt.nix
+++ treefmt.nix
@@ -8,7 +8,7 @@
       # Used to find the project root
       projectRootFile = "flake.lock";
 
-      programs.deno.enable = !pkgs.stdenv.buildPlatform.isRiscv64;
+      programs.deno.enable = !pkgs.stdenv.buildPlatform.isRiscV64;
       programs.rustfmt.enable = true;
 
       settings.formatter.nix = {
