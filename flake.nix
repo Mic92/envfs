914cacda0dc6b5904912a8ce5e81e5d303205b5a
Jörg Thalheim <joerg@thalheim.io>
2023-11-24 10:36:36 +0100

    add treefmt

--- flake.nix
+++ flake.nix
@@ -4,9 +4,12 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
+  inputs.treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } ({self, ...}: {
+    flake-parts.lib.mkFlake { inherit inputs; } ({ self, ... }: {
+      imports = [ ./treefmt.nix ];
       systems = [
         "x86_64-linux"
         "aarch64-linux"
---
+++ treefmt.nix
@@ -0,0 +1,45 @@
+{ lib, inputs, ... }: {
+  imports = [
+    inputs.treefmt-nix.flakeModule
+  ];
+
+  perSystem = { pkgs, ... }: {
+    treefmt = {
+      # Used to find the project root
+      projectRootFile = "flake.lock";
+
+      programs.deno.enable = true;
+      programs.rustfmt.enable = true;
+
+      settings.formatter = {
+        nix = {
+          command = "sh";
+          options = [
+            "-eucx"
+            ''
+              # First deadnix
+              ${lib.getExe pkgs.deadnix} --edit "$@"
+              # Then nixpkgs-fmt
+              ${lib.getExe pkgs.nixpkgs-fmt} "$@"
+            ''
+            "--"
+          ];
+          includes = [ "*.nix" ];
+          excludes = [ "nix/sources.nix" ];
+        };
+
+        clippy = {
+          command = "sh";
+          options = [
+            "-eucx"
+            ''
+              cargo clippy --fix
+            ''
+            "--"
+          ];
+          includes = [ "*.rs" ];
+        };
+      };
+    };
+  };
+}
